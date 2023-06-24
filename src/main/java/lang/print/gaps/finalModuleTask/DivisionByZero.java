7e2d17c7782304d50977e7988fce56f66b2eb323
Axrich <116589250+Axrich@users.noreply.github.com>
2023-06-24 13:00:15 +0500

    Update DivisionByZero.java

--- src/main/java/lang/print/gaps/finalModuleTask/DivisionByZero.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DivisionByZero.java
@@ -2,10 +2,11 @@ package lang.print.gaps.finalModuleTask;
 
 public class DivisionByZero {
     public static void main(String[] args) {
-        Double nu0mber = 10;
-        if (number !=0.0) {
-            System.out.println(number/0);
+        Double number = 10.0;
+        if (number != 0.0) {
+            System.out.println(number / 0);
         } else {
-        System.out.println("Cannot devide by zero");
+            System.out.println("Cannot divide by zero");
+        }
     }
 }

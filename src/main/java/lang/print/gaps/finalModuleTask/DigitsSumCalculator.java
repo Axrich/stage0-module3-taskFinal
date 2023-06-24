1286e8a60a2e062c95bb054cd3989045afcf8222
Axrich <116589250+Axrich@users.noreply.github.com>
2023-06-24 12:56:00 +0500

    Update DigitsSumCalculator.java

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,11 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int sum = 0;
+        sum += number % 10;
+        sum += (numer / 10) % 10;
+        sum += (number / 100)% 10;
+        sim += (number / 1000);
+        System.out.println("sum");
     }
 }
